b6a7d862339bbb6f201ec60d608acd6803099eed
Jeongkyun Kim <dev.jkkim@gmail.com>
2021-02-04 01:30:30 +0900

    Update APIModel.swift

--- watsup/API/APIModel.swift
+++ watsup/API/APIModel.swift
@@ -103,7 +103,7 @@ enum APIModel: URLRequestConvertible {
     
     
     func asURLRequest() throws -> URLRequest {
-        let url = try "http://dev.team726.com".asURL()
+        let url = try "http://dev.team726.com:8000".asURL()
         var urlRequest = URLRequest(url: url.appendingPathComponent(path))
         
         // HTTP Method
